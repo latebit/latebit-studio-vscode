add636d5114a47244edde2c60717e4fd1e772d22
shikaan <spagnolo.manu@gmail.com>
2024-04-28 23:26:00 +0200

    feat: visually seperate beats

--- views/audio/components/editor.js
+++ views/audio/components/editor.js
@@ -3,11 +3,27 @@ import { executeHostCommand } from '../ipc.js';
 
 export const $editor = {
   $root: document.getElementById('editor'),
-  init() { },
+  init() {
+    state.listen('tune', (tune) => this.update(tune));
+  },
   update(tune) {
     const tracks = tune.getTracksCount()
     const maxTrackLength = tune.getBeatsCount() * tune.getTicksPerBeat();
     const $root = this.$root.cloneNode(false);
+
+    // Add an index column 
+    const $indexColumn = document.createElement('div');
+    for (let j = 0; j < maxTrackLength; j++) {
+      const $index = document.createElement('div');
+      $index.appendChild(document.createTextNode(j));
+      $index.classList.add('index');
+      if (j % tune.getTicksPerBeat() === 0) {
+        $index.classList.add('beat');
+      }
+      $indexColumn.appendChild($index);
+    }
+    $root.appendChild($indexColumn);
+
     for (let i = 0; i < tracks; i++) {
       const $track = document.createElement('div');
       for (let j = 0; j < maxTrackLength; j++) {
@@ -17,15 +33,18 @@ export const $editor = {
       $root.appendChild($track);
     }
     this.$root.replaceWith($root);
+    this.$root = $root;
   }
 }
 
 const makeCell = (tune, track, tick) => {
   const $cell = document.createElement('input');
-  $cell.setAttribute('data-track', track);
-  $cell.setAttribute('data-tick', tick);
   $cell.value = Module.getNote(tune, track, tick).getSymbol();
   $cell.classList.add('cell');
+
+  if (tick % tune.getTicksPerBeat() === 0) {
+    $cell.classList.add('beat');
+  }
 
   let previousValue = $cell.value;
   $cell.addEventListener('keyup', (e) => {
