4bc26fd93568468326e517e0be2a64e157ba84fe
shikaan <spagnolo.manu@gmail.com>
2024-05-01 15:31:14 +0200

    feat: allow new tune

--- views/audio/app.js
+++ views/audio/app.js
@@ -6,7 +6,7 @@ import { $metadata } from './components/metadata.js'
 import { $playback } from './components/playback.js'
 import { $editor } from './components/editor.js'
 import { $edit } from './components/edit.js'
-import { TuneParser } from './sid.js'
+import { Tune, TuneParser } from './sid.js'
 
 const $app = {
   init() {
@@ -20,6 +20,18 @@ const $app = {
        * @type {(payload: string) => void}
        */
       const onResponse = (payload) => {
+        if (!payload.trim()) {
+          const tune = new Tune(3);
+          tune.setBeatsCount(4);
+          tune.setBpm(90);
+          tune.setTicksPerBeat(4);
+          state.setTune(tune);
+          $playback.$play.disabled = false;
+          $playback.$loop.disabled = false;
+          $playback.$stop.disabled = false;
+          return;
+        }
+
         try {
           const tune = TuneParser.fromString(payload);
           state.setTune(tune);
--- views/audio/sid.js
+++ views/audio/sid.js
@@ -5,19 +5,6 @@
  */
 
 const sid = window.Module;
-
-/** @type {sid.Player} */
-const Player = window.Module.Player;
-
-/** @type {sid.TuneParser} */
-const TuneParser = window.Module.TuneParser;
-
-/** @type {sid.Tune} */
-class Tune extends window.Module.Tune { };
-
-/** @type {sid.Note} */
-class Note extends window.Module.Note { };
-
-const { getNote, setNote, getTrackSize, removeNote } = sid;
+const { Player, TuneParser, Tune, Note, getNote, setNote, getTrackSize, removeNote } = sid;
 
 export { Player, TuneParser, Tune, Note, getNote, getTrackSize, removeNote, setNote }
