ab1171d95791bc96749139465dbf80b91a1543d0
shikaan <spagnolo.manu@gmail.com>
2024-06-13 20:56:54 +0200

    chore: use radios for tools

--- views/sprites/components/tools.js
+++ views/sprites/components/tools.js
@@ -11,26 +11,22 @@ export const $tools = {
     Object.values(Color).forEach((color) => {
       if (color.value === Color.UNDEFINED_COLOR.value || typeof color.value != 'number') return;
 
-      const $color = document.createElement('button');
+      const $color = document.createElement('input');
+      $color.type = 'radio';
       $color.classList.add('color');
+      $color.name = 'color';
+      $color.checked = color === state.getCurrentColor();
       $color.style.backgroundColor = COLOR_TO_HEX[color.value];
-      $color.addEventListener('click', this.selectCurrentColor.bind(this, color));
+      $color.value = color.value.toString();
       this.$palette.appendChild($color);
     })
 
-    state.listen('currentColor', this.highlightCurrentColor.bind(this));
-  },
-  selectCurrentColor(/** @type {Color} */ color, /** @type {Event} */ e) {
-    e.preventDefault();
-    state.setCurrentColor(color);
-  },
-  highlightCurrentColor(/** @type {Color} */ color) {
-    const $selected = this.$palette.querySelector('.selected');
-    if ($selected) {
-      $selected.classList.remove('selected');
-    }
+    this.$palette.addEventListener('change', (e) => {
+      const $color = /** @type {HTMLInputElement} */ (e.target);
+      const color = Object.values(Color).find((color) => color.value === parseInt(($color.value)));
 
-    const $color = this.$palette.children[color.value];
-    $color?.classList.add('selected');
+      if (!color) return;
+      state.setCurrentColor(color);
+    })
   }
 }
